a4c47837929a48ae680a8696247d83d136564250
hddev244 <daivvh244@gmail.com>
2024-06-05 18:41:14 +0700

    fix: link css

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles.css";
 import { Toaster } from "sonner";
 import { cookies } from "next/headers";
 import { ThemeProvider } from "@/components/theme-provider";
